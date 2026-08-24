c6a9f1a1da97865384ed530e94a251e30f845560
Alexander Medved <medved1991alexander@gmail.com>
2026-08-24 12:47:06 +0300

    HospitalApiRequests

---
+++ Client/HospitalApiRequests.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp.Models;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace Client;
+
+public static class HospitalApiRequests
+{
+    static private readonly HttpClient Client = new HttpClient();
+
+    static HospitalApiRequests()
+    {
+        Client.BaseAddress = new Uri(AppConfig.HospitalRoutes.BaseUrl);
+        Client.DefaultRequestHeaders.Accept.Clear();
+        Client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+
+    public static async Task PostBatch(List<Patient> patients)
+    {
+        string path = AppConfig.HospitalRoutes.Endpoints.PatientBatch;
+
+        try
+        {
+            var response = await Client.PostAsJsonAsync(path, patients);
+            response.EnsureSuccessStatusCode();
+
+            //change console text to event
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("[SUCCESS] The patient batch was successfully sent and processed by the API.");
+            Console.ResetColor();
+
+        }
+        catch (HttpRequestException ex) 
+        {
+            //change console text to event
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[ERROR] API request error. Status: {ex.StatusCode}. Message: {ex.Message}");
+            Console.ResetColor();
+
+            throw;
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,62 +1,18 @@
-﻿using ConsoleApp.Models;
-using ConsoleClient;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-
+﻿using ConsoleClient;
 
 
 namespace Client;
 
+
+
 class Program
 {
-    static readonly HttpClient Client = new HttpClient();
-
-
     static async Task Main(string[] args)
     {
+        var patients = PtientCreator.CreatePatients(1);
 
-        string baseUrl = AppConfig.HospitalRoutes.BaseUrl;
-        string patientBatch = AppConfig.HospitalRoutes.Endpoints.PatientBatch;
-
-        Client.BaseAddress = new Uri(baseUrl);
-        Client.DefaultRequestHeaders.Accept.Clear();
-        Client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-
-        var patientList = PtientCreator.CreatePatients(100);
-
-        await PostBatch(patientList, patientBatch);
+        await HospitalApiRequests.PostBatch(patients);
 
         Console.ReadLine();       
-    }
-
-
-
-    private static async Task PostBatch(List<Patient> patients, string endpoint)
-    {
-        
-        if (!patients.Any())
-            Console.WriteLine("The patient list is empty");// переписать на эксепшен
-
-        if (string.IsNullOrEmpty(endpoint))
-            Console.WriteLine("Request path not provided");// переписать на эксепшен
-
-
-        try
-        {
-            var response = await Client.PostAsJsonAsync(endpoint, patients);
-            response.EnsureSuccessStatusCode();
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Adding patients successfully! Status code {response.StatusCode}");
-
-        }
-        catch (Exception ex)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Error.WriteLine($"Adding patients error: {ex.Message}");
-        }
-
-        Console.ResetColor();
     }
 }
